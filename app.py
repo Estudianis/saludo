c0e9a74b596c4177f4fb19397789be5fa0d265c6
Estudianis <44418633+Estudianis@users.noreply.github.com>
2018-10-24 17:19:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
  
 @app.route("/")
 def hello():
     return "Hola como estas :D"
 
-if __name__ == "__main__":
-    app.run()
+ @app.route("/index")
+def index():
+    return open("index.html").read()
 
+@app.route("/suma")
+def suma():
+    n1 = request.args.get('n1')
+    n2 = request.args.get('n2')
+    restun n1 + n2
+    
+    @app.route("/resta")
+def resta():
+    n1 = request.args.get('n1')
+    n2 = request.args.get('n2')
+    restun str(int(n1) - int(n2))
+   
 @app.route("/index")
 def index():
     return open("index.html").read()
  
-
+ if __name__ == "__main__":
+    app.run()
