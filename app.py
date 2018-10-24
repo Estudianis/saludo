39ae8fda88877d18965839e30b7923e86e7bfd8d
Estudianis <44418633+Estudianis@users.noreply.github.com>
2018-10-24 16:57:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask
 app = Flask(__name__)
  
 @app.route("/")
 def hello():
     return "Hola como estas :D"
- 
+
+if__name__ == "__main__":
+   app.run()
+
 @app.route("/index")
 def index():
     return open("index.html").read()
  
-if__name__ == "__main__":
-   app.run()
+
